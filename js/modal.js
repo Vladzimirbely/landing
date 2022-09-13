092b6ddd54eda68f092aba690e2ce058c172cb1a
VladzimirBely <vladzimir.bely@gmail.com>
2022-09-13 12:38:06 +0300

    fix design buttons and modal window

--- js/modal.js
+++ js/modal.js
@@ -5,20 +5,24 @@ const closeModalButton = document.querySelector('.close');
 
 openModalButton.addEventListener('click', function (e) {
     modal.classList.add('open');
+
     let time = document.getElementById('date');
     function getTime () {
         return new Date().toString();
     }
+
     setInterval (
         () => time.innerHTML = getTime(), 1000);
 });
 
 confirmModalButton.addEventListener('click', function (e) {
     modal.classList.remove('open');
+    
     let time2 = document.getElementById('additional-txt');
     function getTime () {
         return new Date().toString();
     }
+
     setInterval (
         () => time2.innerHTML = getTime(), 1000);
 });
